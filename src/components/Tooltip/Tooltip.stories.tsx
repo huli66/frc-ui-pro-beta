cd3abd67782db0b5ec7a437f5fbf148fcb023f1d
吴鑫 <xin.wu@sumscope.com>
2022-07-05 17:42:57 +0800

    tooltip组件

---
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,188 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+import Button from "../Button";
+import Icon from "../Icon";
+import { ComponentMeta } from '@storybook/react';
+
+
+import {
+    Title,
+    Description,
+    ArgsTable,
+    Stories,
+    Heading,
+    Subheading
+} from '@storybook/addon-docs';
+
+import Tooltip from './index';
+
+// ----------------------------------------------------------------
+
+// 引用示例代码
+const ImportComponent = () => {
+    const markdown = `
+~~~js
+import { Tooltip } from 'frc-ui-pro';
+~~~
+`
+
+    return <>
+        <ReactMarkdown children={markdown} components={{
+            code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '')
+                return !inline && match ? (
+                    <SyntaxHighlighter
+                        children={String(children).replace(/\n$/, '')}
+                        style={tomorrow}
+                        language={match[1]}
+                        PreTag="div"
+                        {...props}
+                    />
+                ) : (
+                    <code className={className} {...props}>
+                        {children}
+                    </code>
+                )
+            }
+        }} /></>
+}
+
+// ----------------------------------------------------------------
+
+export default {
+    title: 'Tooltip 文字提示',
+    component: Tooltip,
+    parameters: {
+        docs: {
+            // docs 页面 => 总体布局
+            page: () => (
+                <>
+                    <Title />
+                    <Description>主动触发（鼠标跟随Tips）</Description>
+                    <ImportComponent />
+                    <Stories title="组件总览" includePrimary={true} />
+
+                    <Heading>API</Heading>
+                    <Subheading>属性</Subheading>
+
+                    <Subheading>Tooltip</Subheading>
+                    <ArgsTable of={Tooltip} />
+                </>
+            ),
+        },
+    },
+} as ComponentMeta<typeof Tooltip>;
+
+// ----------------------------------------------------------------
+
+export const _ActiveComponent = () => {
+    return (<>
+        <div className='center'>
+            <div className='top_panel'>
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='topLeft'>
+                    <span className='tooltip-base'>上左</span>
+                </Tooltip>
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='top'>
+                    <span className='tooltip-base'>上中</span>
+                </Tooltip>
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='topRight'>
+                    <span className='tooltip-base'>上右</span>
+                </Tooltip>
+            </div>
+            <div className='left_panel'>
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='left'>
+                    <span className='tooltip-base'>左</span>
+                </Tooltip>
+            </div>
+            <div className='right_panel'>
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='right'>
+                    <span className='tooltip-base'>右</span>
+                </Tooltip>
+            </div>
+            <div className='bottom_panel'>
+
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='bottomLeft'>
+                    <span className='tooltip-base'>下左</span>
+                </Tooltip>
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='bottom'>
+                    <span className='tooltip-base'>下中</span>
+                </Tooltip>
+                <Tooltip type="active" content='这里是提示文本' hasArrow={true} placement='bottomRight'>
+                    <span className='tooltip-base'>下右</span>
+                </Tooltip>
+            </div>
+            <Tooltip type="active" hasArrow={false} content={<div><div><span>提示文本</span><Button>按钮</Button><Icon type="frown" /></div> <div><span style={{ background: '#263735' }}>估值收益率(%)</span><span style={{ color: '#F9C152' }}>2.3700</span></div></div>}>
+                <div className='tooltip-base no-arrow'>
+                    <span>无箭头</span>
+                </div>
+            </Tooltip>
+        </div >
+    </>)
+};
+
+_ActiveComponent.storyName = '主动触发 tooltip';
+_ActiveComponent.parameters = {
+    controls: { hideNoControlsWarning: true },
+};
+
+// ----------------------------------------------------------------
+
+export const _PassiveComponent = () => {
+    const [isTooltip1Visible, setIsTooltip1Visible] = useState(false);
+    const [isTooltip2Visible, setIsTooltip2Visible] = useState(false);
+    const duration = 2000;//消息框持续时间,ms
+
+    const showTooltip1 = () => {
+        setIsTooltip1Visible(true);
+    };
+
+    const closeToolTip1 = () => {
+        setIsTooltip1Visible(false);
+    };
+
+    const showTooltip2 = () => {
+        setIsTooltip2Visible(true);
+        let timer = setTimeout(
+            () => {
+                setIsTooltip2Visible(false)
+            },
+            duration
+        );
+    };
+    return (<>
+        <div className='center'>
+            <Tooltip
+                type="passive"
+                content={<div><span>提示文本、提示文本 点击确定关闭提示</span><Button type="gray" style={{ margin: '0 0 0 10px' }} onClick={closeToolTip1}>确定</Button></div>}
+                hasArrow={true}
+                placement='left'
+                visible={isTooltip1Visible}>
+                <Button type="primary" onClick={showTooltip1}>
+                    有箭头
+                </Button>
+            </Tooltip>
+
+            <Button type="primary" style={{ margin: '0 0 0 20px' }} onClick={showTooltip2}>
+                无箭头
+            </Button>
+            <Tooltip
+                type="passive"
+                content='系统提示文本'
+                hasArrow={false}
+                placement='right'
+                visible={isTooltip2Visible}
+            >
+                <span></span>
+            </Tooltip>
+        </div>
+
+    </>)
+};
+
+_PassiveComponent.storyName = '被动触发 tooltip';
+_PassiveComponent.parameters = {
+    controls: { hideNoControlsWarning: true },
+};
---
+++ src/components/Tooltip/tooltip.tsx
@@ -0,0 +1,67 @@
+import React, { FC, useRef } from 'react'
+import classNames from 'classnames'
+import RcTooltip from 'rc-tooltip/lib/Tooltip'
+import { TooltipProps as RcTooltipProps } from 'rc-tooltip/lib/Tooltip'
+import { message } from 'antd'
+
+export type ToolTipType = 'active' | 'passive'
+export type ToolTipArrowContent = '' | '{<div className="rc-tooltip-arrow-inner"></div>}'
+export type ToolTipPlacementType = 'left' | 'right' | 'top' | 'bottom' | 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight'
+
+interface BaseTooltipProps {
+  /** 设置提示主动或被动触发 */
+  type?: ToolTipType
+  /** 设置提示框内容 */
+  content?: React.ReactNode
+  /** 设置提示优先显示位置*/
+  placement?: ToolTipPlacementType
+  /** 设置提示框是否携带箭头 */
+  hasArrow?: boolean
+  /** 设置提示是否可见 */
+  visible?: boolean
+}
+
+export type FRCTooltipProps = BaseTooltipProps & Omit<RcTooltipProps, 'overlay' | 'placement'>
+
+export const Tooltip: FC<FRCTooltipProps> = (props) => {
+  const {
+    overlayClassName,
+    type,
+    content,
+    placement,
+    hasArrow,
+    children,
+    ...restProps
+  } = props
+
+  const classes = classNames('frc-tooltip', overlayClassName, {
+    [`frc-tooltip-${type}`]: type,
+    [`frc-tooltip-without-arrow`]: !hasArrow,
+    [`frc-tooltip-placement-${placement}`]: placement,
+    [`frc-tooltip-passive-without-arrow`]: type === 'passive' && !hasArrow,
+  })
+
+  const options = {
+    overlayClassName: classes,
+    overlay: content,
+    arrowContent: (!hasArrow ? null : <div className="rc-tooltip-arrow-inner"></div>),
+    placement: placement,
+    ...restProps,
+  }
+
+  // main
+  return (
+    <RcTooltip {...options}>
+      {children}
+    </RcTooltip>
+  )
+}
+
+// normal
+Tooltip.defaultProps = {
+  type: 'active',
+  placement: 'right',
+  hasArrow: true,
+}
+
+export default Tooltip
