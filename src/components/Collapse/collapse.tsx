55dc9fd1439127b6060493dbc7be7202ea77e989
siyuanyuu <siyuan.yu@sumscope.com>
2022-07-06 10:25:56 +0800

    feat-collapse: v1.0.1

--- src/components/Collapse/collapse.tsx
+++ src/components/Collapse/collapse.tsx
@@ -13,28 +13,30 @@ export const Collapse: FC<ICollapseProps> = (props) => {
             ...style
         }
         style.height = topHeight + 'px'
-        console.log('style.height',style.height);
+        console.log('style.height',topHeight);
         
         return style
     }
     function changeHeightStart(el:any) {
-        const hrbox = document.getElementById('frc-hrbox')
+        const hrbox = document.getElementById('frc-hrbox')   
         if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))){
             const diff = el.changedTouches[0].clientY - hrbox!.offsetTop
             document.ontouchmove = function(e) {
-                setTopHeight(e.changedTouches[0].clientY - diff)
+                let newTopHeight = e.changedTouches[0].clientY - diff
+                if ( hrbox!.parentElement!.offsetHeight > newTopHeight && newTopHeight > 0) {
+                    setTopHeight(newTopHeight)
+                }
                 document.onmouseup = function () {
                     document.onmousemove = null  
                 }
             }
         } else{
             let diff = el.clientY - hrbox!.offsetTop
-            console.log('diff',diff,el.clientY,hrbox!.offsetTop);
-            
             document.onmousemove = function(e) {
-                setTopHeight(e.clientY - diff)
-                console.log('clientY',e.clientY);
-                
+                let newTopHeight = e.clientY - diff
+                if ( hrbox!.parentElement!.offsetHeight > newTopHeight && newTopHeight > 0) {
+                    setTopHeight(newTopHeight)
+                }
                 document.onmouseup = function () {
                     document.onmousemove = null     
                     diff = el.clientY - hrbox!.offsetTop
@@ -47,11 +49,13 @@ export const Collapse: FC<ICollapseProps> = (props) => {
         <div className={`${className} frc-collapse-drawer`}>
             <div className='content-container'>
                 <div id='frc-topbox' className='frc-collapse-drawer-top' style={getTopHeight()}>
+                    上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子上面盒子
                 </div>
                 <div id='frc-hrbox' className='frc-collapse-drawer-hr' onMouseDown={changeHeightStart} onTouchStart={ changeHeightStart }>
                     <div className='frc-collapse-drawer-hr-arr'></div>
                 </div>
                 <div className='frc-collapse-drawer-bottom'>
+                    下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子下面盒子
                 </div>
             </div>
         </div>
