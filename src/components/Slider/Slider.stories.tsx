c485a8ae2df80e79078002be5d5fdebae157233d
suixian.gong <suixian.gong@sumscope.com>
2022-09-08 11:36:46 +0800

    feat: slider

---
+++ src/components/Slider/Slider.stories.tsx
@@ -0,0 +1,235 @@
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+import { ComponentMeta } from '@storybook/react'
+
+import {
+  Title,
+  Description,
+  ArgsTable,
+  Stories,
+  Heading,
+  Subheading,
+} from '@storybook/addon-docs'
+
+import { Row, Col } from 'antd'
+import Slider, { FRCSliderProps } from './index'
+import InputNumber from '../InputNumber'
+import Switch from '../Switch'
+
+// ----------------------------------------------------------------
+
+// 引用示例代码
+const ImportComponent = () => {
+  const markdown = `
+~~~js
+import { Slider } from 'frc-ui-pro';
+~~~
+
+`
+
+  return (
+    <>
+      <ReactMarkdown
+        children={markdown}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={tomorrow}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      />
+    </>
+  )
+}
+
+// ----------------------------------------------------------------
+
+export default {
+  title: '数据录入/Slider 滑动控制器',
+  component: Slider,
+  parameters: {
+    docs: {
+      // docs 页面 => 总体布局
+      page: () => (
+        <>
+          <Title />
+          <Description>滑动型输入器，展示当前值和可选范围。</Description>
+          <ImportComponent />
+          <Stories title="组件总览" includePrimary={true} />
+
+          <Heading>API</Heading>
+          <Subheading>属性</Subheading>
+
+          <Subheading>Slider</Subheading>
+          <ArgsTable of={Slider} />
+        </>
+      ),
+    },
+  },
+  // 细分属性 - 分类（用于canvas 页查阅）
+} as ComponentMeta<typeof Slider>
+
+// -------------------------------------------------------------------
+
+export const Default = (args: FRCSliderProps) => <Slider {...args} />
+
+Default.storyName = '默认 Slider'
+
+// -----------------------------------------------------------------
+
+export const SizeComponent = () => {
+  return (
+    <>
+      <Slider defaultValue={30} />
+      <Slider defaultValue={30} size="large" style={{ margin: '70px 0' }} />
+      <Slider defaultValue={30} size="small" />
+      <Slider range defaultValue={[20, 50]} />
+    </>
+  )
+}
+SizeComponent.storyName = '大小 Slider'
+SizeComponent.parameters = {
+  controls: { hideNoControlsWarning: true }
+}
+
+// -------------------------------------------------------------------
+
+export const MarksSliderComponent = () => {
+  const marks = {
+    0: '0',
+    26: '26',
+    37: '37',
+    100: {
+      style: {
+        color: '#f50',
+      },
+      label: <strong>100</strong>,
+    },
+  }
+  const style = { color: '#FFEBC8' }
+  return (
+    <>
+      <h4 style={style}>included=true</h4>
+      <Slider marks={marks} defaultValue={37} />
+      <Slider range marks={marks} defaultValue={[26, 37]} />
+
+      <h4 style={style}>included=false</h4>
+      <Slider marks={marks} included={false} defaultValue={37} />
+
+      <h4 style={style}>marks & step</h4>
+      <Slider marks={marks} step={10} defaultValue={37} />
+
+      <h4 style={style}>step=null</h4>
+      <Slider marks={marks} step={null} defaultValue={37} />
+    </>
+  )
+}
+
+MarksSliderComponent.storyName = '带标签的滑块 Slider'
+MarksSliderComponent.parameters = {
+  controls: { hideNoControlsWarning: true }
+}
+
+// -------------------------------------------------------------------
+
+export const MarksTypeSliderComponent = () => {
+  const [value, setValue] = useState<[number, number]>([25, 80]);
+  const [valu, setValu] = useState<number>(80);
+  const marks = {
+    0: '0',
+    25: '25',
+    50: '50',
+    75: '75',
+    100: '100',
+  }
+  const handleChange = (val: [number, number]) => {
+    setValue(val);
+  }
+  const handle = (val: number) => {
+    setValu(val);
+  }
+  return (
+    <>
+      <Slider range marks={marks} type='small' defaultValue={[25, 50]} />
+      <Slider range marks={marks} type='small' defaultValue={[25, 40]} />
+      <Slider range marks={marks} type='small' defaultValue={[25, 50]} />
+      <Slider marks={marks} type='small' defaultValue={50} onChange={handle} value={valu} />
+      <Slider range marks={marks} type='small' value={value} onChange={handleChange} />
+    </>
+  )
+}
+
+MarksTypeSliderComponent.storyName = '大小滑块 Slider'
+MarksTypeSliderComponent.parameters = {
+  controls: { hideNoControlsWarning: true }
+}
+
+// -----------------------------------------------------------------
+
+export const VerticalSliderComponent = () => {
+  const style = {
+    display: 'inline-block',
+    height: 300,
+    marginLeft: 70
+  }
+
+  const marks = {
+    0: '0',
+    26: '26',
+    37: '37',
+    100: {
+      style: {
+        color: '#f50',
+      },
+      label: <strong>100</strong>,
+    },
+  }
+  return (
+    <>
+      <div style={style}>
+        <Slider vertical defaultValue={30} />
+      </div>
+      <div style={style}>
+        <Slider vertical range step={10} defaultValue={[20, 50]} />
+      </div>
+      <div style={style}>
+        <Slider vertical range marks={marks} defaultValue={[26, 37]} />
+      </div>
+      <div style={style}>
+        <Slider vertical size='small' range marks={marks} defaultValue={[26, 37]} />
+      </div>
+      <div style={style}>
+        <Slider vertical size='large' range marks={marks} defaultValue={[26, 37]} />
+      </div>
+      <div style={style}>
+        <Slider vertical type='small' range marks={marks} defaultValue={[26, 37]} />
+      </div>
+      <div style={style}>
+        <Slider vertical type='small' size='small' range marks={marks} defaultValue={[26, 37]} />
+      </div>
+      <div style={style}>
+        <Slider vertical type='small' size='large' range marks={marks} defaultValue={[26, 37]} />
+      </div>
+    </>
+  )
+}
+
+VerticalSliderComponent.storyName = '垂直方向的 Slider'
+VerticalSliderComponent.parameters = {
+  controls: { hideNoControlsWarning: true }
+}
