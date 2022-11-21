98e4cf258bdff5fcde6b0eb13e0bcfa795f261d7
hui.chen <hui.chen@sumscope.com>
2022-11-21 15:11:31 +0800

    feat: add business composite module -> iteration

--- src/components/_BusinessCompositeModule/CustomButton/CustomButton.stories.tsx
+++ src/components/_BusinessCompositeModule/CustomButton/CustomButton.stories.tsx
@@ -8,10 +8,20 @@ import {
     Stories,
     Heading,
     Subheading,
+    Source
 } from '@storybook/addon-docs';
 
-import { ImportCode } from "../../../utils/importComponent";
 import CustomButton, { CustomButtonProps } from './customButton';
+
+// --------------------------------------------------------------
+
+const importCode = `
+// import code
+import { CustomButton } from 'frc-ui-pro';
+import { CustomButtonProps } from "frc-ui-pro/components/_BusinessCompositeModule/CustomButton";
+`;
+
+// --------------------------------------------------------------
 
 export default {
     title: '业务模块/Custom Button ',
@@ -23,6 +33,12 @@ export default {
                 <>
                     <Title />
                     <Description>业务组件：自定义按钮</Description>
+                    <Description>其他描述...................</Description>
+                    <Source
+                        dark
+                        language="ts"
+                        code={importCode}
+                    />
                     <Stories title="组件总览" includePrimary={true} />
 
                     <Heading>API</Heading>
@@ -40,23 +56,11 @@ export default {
 // ----------------------------------------------------------------
 
 export const Default = () => {
-
-    // --------------------------------------------------------------
-
-    const code = `
-        // import code
-        import { CustomButton } from 'frc-ui-pro';
-        import { ColumnsTypeProps } from "frc-ui-pro/components/Table/table";
-      `;
-
-    // --------------------------------------------------------------
-
     const option: CustomButtonProps = {
         type: 'lead',
     }
 
     return <>
-        <ImportCode code={code} />
         <CustomButton {...option}>Custom Button</CustomButton>
     </>
 };
