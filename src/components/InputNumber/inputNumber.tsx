f61249ac68568ed474989e6c86d249b9f8426339
hui.chen <hui.chen@sumscope.com>
2023-02-17 11:06:27 +0800

    fix: inputNumber -> ts

--- src/components/InputNumber/inputNumber.tsx
+++ src/components/InputNumber/inputNumber.tsx
@@ -1,102 +1,119 @@
-import React, { useState, forwardRef } from 'react'
-import classNames from 'classnames'
-import AntdInputNumber, { InputNumberProps } from 'antd/es/input-number'
+import React, { useState, forwardRef } from "react";
+import classNames from "classnames";
+import AntdInputNumber, { InputNumberProps } from "antd/es/input-number";
 export interface BaseInputNumberProps extends InputNumberProps {
   /** 带标签的 input，设置后置标签 */
-  addonAfter?: React.ReactNode
+  addonAfter?: React.ReactNode;
   /** 带标签的 input，设置前置标签 */
-  addonBefore?: React.ReactNode
+  addonBefore?: React.ReactNode;
   /** 自动获取焦点 */
-  autoFocus?: boolean
+  autoFocus?: boolean;
   /** 是否有边框 */
-  bordered?: boolean
+  bordered?: boolean;
   /** 是否显示增减按钮 */
-  controls?: boolean
+  controls?: boolean;
   /** 小数点 */
-  decimalSeparator?: string
+  decimalSeparator?: string;
   /** 初始值 */
-  defaultValue?: number | string
+  defaultValue?: number | string;
   /** 禁用 */
-  disabled?: boolean
+  disabled?: boolean;
   /** 指定输入框展示值的格式 */
-  formatter?: (value?: number | string, info?: { userTyping: boolean, input: string }) => string
+  formatter?: (
+    value?: number | string,
+    info?: { userTyping: boolean; input: string }
+  ) => string;
   /** 是否启用键盘快捷行为 */
-  keyboard?: boolean
+  keyboard?: boolean;
   /** 最大值 */
-  max?: number | string
+  max?: number | string;
   /** 最小值 */
-  min?: number | string
+  min?: number | string;
   /** 指定从 formatter 里转换回数字的方式，和 formatter 搭配使用 */
-  parser?: (string: string | undefined) => string | number
+  parser?: (string: string | undefined) => string | number;
   /** 数值精度，配置 formatter 时会以 formatter 为准 */
-  precision?: number
+  precision?: number;
   /** 只读 */
-  readOnly?: boolean
+  readOnly?: boolean;
   /** 带有前缀图标的 input */
-  prefix?: React.ReactNode
+  prefix?: React.ReactNode;
   /** 每次改变步数，可以为小数 */
-  step?: number | string
+  step?: number | string;
   /** 字符值模式，开启后支持高精度小数。同时 onChange 将返回 string 类型 */
-  stringMode?: boolean
+  stringMode?: boolean;
   /** 当前值 */
-  value?: number
+  value?: number | string | null;
   /** 变化回调 */
-  onChange?: (value: number | string | null) => void
+  onChange?: (value: number | string | null) => void;
   /** 按下回车的回调 */
-  onPressEnter?: (e: React.KeyboardEvent<HTMLInputElement>) => void
+  onPressEnter?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   /** 点击上下箭头的回调 */
-  onStep?: (value: number | string, info: { offset: number | string, type: 'up' | 'down' } | null) => void
+  onStep?: (
+    value: number | string,
+    info: { offset: number | string; type: "up" | "down" } | null
+  ) => void;
   /** 移除焦点 */
-  blur?: () => void
+  blur?: () => void;
   /** 获取焦点 */
-  focus?: () => void
+  focus?: () => void;
 }
 
-export type FRCInputNumberProps = BaseInputNumberProps
+export type FRCInputNumberProps = BaseInputNumberProps;
 
-export const InputNumber = forwardRef<HTMLInputElement, FRCInputNumberProps>((props, ref) => {
-  const [keyDownEnter, setKeyDownEnter] = useState(false)
-  const [isFocs, setIsFocs] = useState(false)
+export const InputNumber = forwardRef<HTMLInputElement, FRCInputNumberProps>(
+  (props, ref) => {
+    const [keyDownEnter, setKeyDownEnter] = useState(false);
+    const [isFocs, setIsFocs] = useState(false);
 
-  const { className, bordered, disabled, onChange, onKeyDown, onFocus, onBlur, ...restProps } = props
+    const {
+      className,
+      bordered,
+      disabled,
+      onChange,
+      onKeyDown,
+      onFocus,
+      onBlur,
+      ...restProps
+    } = props;
 
-  const classes = classNames('frc-input-number', className, {
-    [`frc-input-number-no-border`]: !bordered,
-    [`frc-input-number-enter`]: keyDownEnter,
-    [`frc-input-number-disabled`]: disabled,
-    [`frc-input-number-focus`]: isFocs,
-  })
+    const classes = classNames("frc-input-number", className, {
+      [`frc-input-number-no-border`]: !bordered,
+      [`frc-input-number-enter`]: keyDownEnter,
+      [`frc-input-number-disabled`]: disabled,
+      [`frc-input-number-focus`]: isFocs,
+    });
 
-  let options = {
-    className: classes,
-    bordered,
-    disabled,
-    ...restProps,
-    onFocus: (e: any) => {
-      onFocus && onFocus(e)
-      setIsFocs(true)
-    },
-    onBlur: (e: any) => {
-      onBlur && onBlur(e)
-      setIsFocs(false)
-    },
-    onKeyDown: (e: any) => {
-      onKeyDown && onKeyDown(e)
-      if (e.code === 'Enter') {
-        setKeyDownEnter(true)
-      }
-    },
-    onChange: (e: any) => {
-      onChange && onChange(e)
-      if (!e && e !== 0) {
-        setKeyDownEnter(false)
-      }
-    },
+    let options = {
+      className: classes,
+      bordered,
+      disabled,
+      ...restProps,
+      onFocus: (e: any) => {
+        onFocus && onFocus(e);
+        setIsFocs(true);
+      },
+      onBlur: (e: any) => {
+        onBlur && onBlur(e);
+        setIsFocs(false);
+      },
+      onKeyDown: (e: any) => {
+        onKeyDown && onKeyDown(e);
+        if (e.code === "Enter") {
+          setKeyDownEnter(true);
+        }
+      },
+      onChange: (e: any) => {
+        onChange && onChange(e);
+        if (!e && e !== 0) {
+          setKeyDownEnter(false);
+        }
+      },
+    };
+
+    // main
+    return <AntdInputNumber ref={ref} {...options} />;
   }
-
-  // main
-  return <AntdInputNumber ref={ref} {...options} />
-})
+);
 
 // normal
 InputNumber.defaultProps = {
@@ -108,7 +125,7 @@ InputNumber.defaultProps = {
   readOnly: false,
   stringMode: false,
   step: 1,
-  placeholder: '请输入',
-}
+  placeholder: "请输入",
+};
 
-export default InputNumber
+export default InputNumber;
