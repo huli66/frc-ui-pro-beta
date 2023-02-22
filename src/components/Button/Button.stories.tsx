3a8b0f38475f075a511c871746df692db9ae3d1a
xin.wu <xin.wu@sumscope.com>
2023-02-22 10:56:33 +0800

    button light

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,297 +1,362 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 import { QuestionCircleOutlined, ProfileOutlined } from "@ant-design/icons";
 
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta } from "@storybook/react";
 
 import {
-    Title,
-    Description,
-    ArgsTable,
-    Stories,
-    Heading,
-    Subheading
-} from '@storybook/addon-docs';
+  Title,
+  Description,
+  ArgsTable,
+  Stories,
+  Heading,
+  Subheading,
+} from "@storybook/addon-docs";
 
-import Button, { FRCButtonProps } from './index';
+import Button, { FRCButtonProps } from "./index";
 
 // ----------------------------------------------------------------
 
 // 引用示例代码
 const ImportComponent = () => {
-    const markdown = `
+  const markdown = `
 ~~~js
 import { Button } from 'frc-ui-pro';
 
 // 按需引入 icon
 import { QuestionCircleOutlined } from "@ant-design/icons";
 ~~~
-`
+`;
 
-    return <>
-        <ReactMarkdown children={markdown} components={{
-            code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || '')
-                return !inline && match ? (
-                    <SyntaxHighlighter
-                        children={String(children).replace(/\n$/, '')}
-                        style={tomorrow}
-                        language={match[1]}
-                        PreTag="div"
-                        {...props}
-                    />
-                ) : (
-                    <code className={className} {...props}>
-                        {children}
-                    </code>
-                )
-            }
-        }} /></>
-}
+  return (
+    <>
+      <ReactMarkdown
+        children={markdown}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={tomorrow}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </>
+  );
+};
 
 // ----------------------------------------------------------------
 
 export default {
-    title: '通用/Button 按钮',
-    component: Button,
-    parameters: {
-        docs: {
-            // docs 页面 => 总体布局
-            page: () => (
-                <>
-                    <Title />
-                    <Description>通过鼠标或键盘，输入范围内的数值。</Description>
-                    <ImportComponent />
-                    <Stories title="组件总览" includePrimary={true} />
+  title: "通用/Button 按钮",
+  component: Button,
+  parameters: {
+    docs: {
+      // docs 页面 => 总体布局
+      page: () => (
+        <>
+          <Title />
+          <Description>通过鼠标或键盘，输入范围内的数值。</Description>
+          <ImportComponent />
+          <Stories title="组件总览" includePrimary={true} />
 
-                    <Heading>API</Heading>
-                    <Subheading>属性</Subheading>
+          <Heading>API</Heading>
+          <Subheading>属性</Subheading>
 
-                    <Subheading>Button</Subheading>
-                    <ArgsTable of={Button} />
-                </>
-            ),
-        },
+          <Subheading>Button</Subheading>
+          <ArgsTable of={Button} />
+        </>
+      ),
     },
-    // 细分属性 - 分类（用于canvas 页查阅）
+  },
+  // 细分属性 - 分类（用于canvas 页查阅）
 } as ComponentMeta<typeof Button>;
 
 // ----------------------------------------------------------------
 
-export const Default = (args: FRCButtonProps) => <Button {...args}>Button</Button>;
+export const Default = (args: FRCButtonProps) => (
+  <Button {...args}>Button</Button>
+);
 
-Default.storyName = '默认 button';
+Default.storyName = "默认 button";
 
 // ----------------------------------------------------------------
 
 export const _ABaseComponent = () => {
-    return (<>
-        <Button type="default">Default Button</Button>
-        <Button type="primary">Primary Button</Button>
-        <Button type="lead">Lead Button</Button>
-        <Button type="gray">Gray Button</Button>
-        <Button type="link" href="https://google.com">
-            Link Button
-        </Button>
-        <Button icon={<ProfileOutlined style={{fontSize:18,paddingTop:3}} />} />
-        <Button type='text'>Text Button</Button>
-        <br />
-        <Button type="default" disabled>Default Button</Button>
-        <Button type="primary" disabled>Primary Button</Button>
-        <Button type="lead" disabled>Lead Button</Button>
-        <Button type="gray" disabled>Gray Button</Button>
-        <Button type="link" href="https://google.com" disabled>
-            Link Button
-        </Button>
-        <Button icon={<ProfileOutlined style={{fontSize:18}} />} disabled />
-        <Button type='text' disabled>Text Button</Button>
-    </>)
+  return (
+    <>
+      <Button type="default">Default Button</Button>
+      <Button type="primary">Primary Button</Button>
+      <Button type="lead">Lead Button</Button>
+      <Button type="gray">Gray Button</Button>
+      <Button type="link" href="https://google.com">
+        Link Button
+      </Button>
+      <Button
+        icon={<ProfileOutlined style={{ fontSize: 18, paddingTop: 3 }} />}
+      />
+      <Button type="text">Text Button</Button>
+      <Button type="light">Light Button</Button>
+      <br />
+      <Button type="default" disabled>
+        Default Button
+      </Button>
+      <Button type="primary" disabled>
+        Primary Button
+      </Button>
+      <Button type="lead" disabled>
+        Lead Button
+      </Button>
+      <Button type="gray" disabled>
+        Gray Button
+      </Button>
+      <Button type="link" href="https://google.com" disabled>
+        Link Button
+      </Button>
+      <Button icon={<ProfileOutlined style={{ fontSize: 18 }} />} disabled />
+      <Button type="text" disabled>
+        Text Button
+      </Button>
+      <Button type="light" disabled>
+        Light Button
+      </Button>
+    </>
+  );
 };
 
-_ABaseComponent.storyName = '基本用法 button';
+_ABaseComponent.storyName = "基本用法 button";
 _ABaseComponent.parameters = {
-    controls: { hideNoControlsWarning: true },
+  controls: { hideNoControlsWarning: true },
 };
 
 // ----------------------------------------------------------------
 
 export const _ActiveComponent = () => {
-    return (<>
-        <Button type="default" work>
-            Default Button
-        </Button>
-        <Button type="default" work workType="high-light">
-            Default Button
-        </Button>
-        <Button type="primary" work>
-            Primary Button
-        </Button>
-        <Button type="lead" work>
-            Lead Button
-        </Button>
-        <Button type="gray" work>
-            Gray Button
-        </Button>
-        <Button type="link" href="https://google.com" work>
-            Link Button
-        </Button>
-        <Button icon={<ProfileOutlined style={{fontSize:18,paddingTop:3}} />} work />
-        <Button type='text' work>Text Button</Button>
-        <br />
-        <Button type="default" work disabled>
-            Default Button
-        </Button>
-        <Button type="default" work workType="high-light" disabled>
-            Default Button
-        </Button>
-        <Button type="primary" work disabled>
-            Primary Button
-        </Button>
-        <Button type="lead" work disabled>
-            Lead Button
-        </Button>
-        <Button type="gray" work disabled>
-            Gray Button
-        </Button>
-        <Button type="link" href="https://google.com" work disabled>
-            Link Button
-        </Button>
-        <Button icon={<ProfileOutlined style={{fontSize:18,paddingTop:3}} />} work disabled />
-        <Button type='text' work disabled>Text Button</Button>
-    </>)
+  return (
+    <>
+      <Button type="default" work>
+        Default Button
+      </Button>
+      <Button type="default" work workType="high-light">
+        Default Button
+      </Button>
+      <Button type="primary" work>
+        Primary Button
+      </Button>
+      <Button type="lead" work>
+        Lead Button
+      </Button>
+      <Button type="gray" work>
+        Gray Button
+      </Button>
+      <Button type="link" href="https://google.com" work>
+        Link Button
+      </Button>
+      <Button
+        icon={<ProfileOutlined style={{ fontSize: 18, paddingTop: 3 }} />}
+        work
+      />
+      <Button type="text" work>
+        Text Button
+      </Button>
+      <Button type="light" work>
+        Light Button
+      </Button>
+      <br />
+      <Button type="default" work disabled>
+        Default Button
+      </Button>
+      <Button type="default" work workType="high-light" disabled>
+        Default Button
+      </Button>
+      <Button type="primary" work disabled>
+        Primary Button
+      </Button>
+      <Button type="lead" work disabled>
+        Lead Button
+      </Button>
+      <Button type="gray" work disabled>
+        Gray Button
+      </Button>
+      <Button type="link" href="https://google.com" work disabled>
+        Link Button
+      </Button>
+      <Button
+        icon={<ProfileOutlined style={{ fontSize: 18, paddingTop: 3 }} />}
+        work
+        disabled
+      />
+      <Button type="text" work disabled>
+        Text Button
+      </Button>
+      <Button type="light" work disabled>
+        Light Button
+      </Button>
+    </>
+  );
 };
 
-_ActiveComponent.storyName = '激活状态 button';
+_ActiveComponent.storyName = "激活状态 button";
 _ActiveComponent.parameters = {
-    controls: { hideNoControlsWarning: true },
+  controls: { hideNoControlsWarning: true },
 };
 
 // ----------------------------------------------------------------
 
 export const _IconComponent = () => {
-    return (<>
-        <Button type="default" icon={<QuestionCircleOutlined />}>
-            Default Button
-        </Button>
-        <Button type="primary" icon={<QuestionCircleOutlined />}>
-            Primary Button
-        </Button>
-        <Button type="lead" icon={<QuestionCircleOutlined />}>
-            Lead Button
-        </Button>
-        <Button type="gray" icon={<QuestionCircleOutlined />}>
-            Gray Button
-        </Button>
-        <Button
-            type="link"
-            href="https://google.com"
-            icon={<QuestionCircleOutlined />}
-        >
-            Link Button
-        </Button>
-        <Button type='text' icon={<QuestionCircleOutlined />}>Text Button</Button>
-        <br />
-        <Button type="default" icon={<QuestionCircleOutlined />} work>
-            Default Button
-        </Button>
-        <Button
-            type="default"
-            icon={<QuestionCircleOutlined />}
-            work
-            workType="high-light"
-        >
-            Default Button
-        </Button>
-        <Button type="primary" icon={<QuestionCircleOutlined />} work>
-            Primary Button
-        </Button>
-        <Button type="lead" icon={<QuestionCircleOutlined />} work>
-            Lead Button
-        </Button>
-        <Button type="gray" icon={<QuestionCircleOutlined />} work>
-            Gray Button
-        </Button>
-        <Button
-            type="link"
-            href="https://google.com"
-            icon={<QuestionCircleOutlined />}
-            work
-        >
-            Link Button
-        </Button>
-        <Button type='text' icon={<QuestionCircleOutlined />} work>Text Button</Button>
-    </>)
+  return (
+    <>
+      <Button type="default" icon={<QuestionCircleOutlined />}>
+        Default Button
+      </Button>
+      <Button type="primary" icon={<QuestionCircleOutlined />}>
+        Primary Button
+      </Button>
+      <Button type="lead" icon={<QuestionCircleOutlined />}>
+        Lead Button
+      </Button>
+      <Button type="gray" icon={<QuestionCircleOutlined />}>
+        Gray Button
+      </Button>
+      <Button
+        type="link"
+        href="https://google.com"
+        icon={<QuestionCircleOutlined />}
+      >
+        Link Button
+      </Button>
+      <Button type="text" icon={<QuestionCircleOutlined />}>
+        Text Button
+      </Button>
+      <Button type="light" icon={<QuestionCircleOutlined />}>
+        Light Button
+      </Button>
+      <br />
+      <Button type="default" icon={<QuestionCircleOutlined />} work>
+        Default Button
+      </Button>
+      <Button
+        type="default"
+        icon={<QuestionCircleOutlined />}
+        work
+        workType="high-light"
+      >
+        Default Button
+      </Button>
+      <Button type="primary" icon={<QuestionCircleOutlined />} work>
+        Primary Button
+      </Button>
+      <Button type="lead" icon={<QuestionCircleOutlined />} work>
+        Lead Button
+      </Button>
+      <Button type="gray" icon={<QuestionCircleOutlined />} work>
+        Gray Button
+      </Button>
+      <Button
+        type="link"
+        href="https://google.com"
+        icon={<QuestionCircleOutlined />}
+        work
+      >
+        Link Button
+      </Button>
+      <Button type="text" icon={<QuestionCircleOutlined />} work>
+        Text Button
+      </Button>
+      <Button type="light" icon={<QuestionCircleOutlined />} work>
+        Light Button
+      </Button>
+    </>
+  );
 };
 
-_IconComponent.storyName = '带图标 button';
+_IconComponent.storyName = "带图标 button";
 _IconComponent.parameters = {
-    controls: { hideNoControlsWarning: true },
+  controls: { hideNoControlsWarning: true },
 };
 
 // ----------------------------------------------------------------
 
 export const _LoadingComponent = () => {
-    return (<>
-        <Button type="default" loading>
-            Default Button
-        </Button>
-        <Button type="primary" loading>
-            Primary Button
-        </Button>
-        <Button type="lead" loading>
-            Lead Button
-        </Button>
-        <Button type="gray" loading>
-            Gray Button
-        </Button>
-        <Button type="link" href="https://google.com" loading>
-            Link Button
-        </Button>
-        <br />
-        <Button type="default" work loading>
-            Default Button
-        </Button>
-        <Button type="primary" work loading>
-            Primary Button
-        </Button>
-        <Button type="primary" work workType="high-light" loading>
-            Primary Button
-        </Button>
-        <Button type="lead" work loading>
-            Lead Button
-        </Button>
-        <Button type="gray" work loading>
-            Gray Button
-        </Button>
-        <Button type="link" href="https://google.com" work loading>
-            Link Button
-        </Button>
-        <br />
-        <Button type="default" work disabled loading>
-            Default Button
-        </Button>
-        <Button type="primary" work disabled loading>
-            Primary Button
-        </Button>
-        <Button type="primary" work disabled workType="high-light" loading>
-            Primary Button
-        </Button>
-        <Button type="lead" work disabled loading>
-            Lead Button
-        </Button>
-        <Button type="gray" work disabled loading>
-            Gray Button
-        </Button>
-        <Button type="link" href="https://google.com" work disabled loading>
-            Link Button
-        </Button>
-    </>)
+  return (
+    <>
+      <Button type="default" loading>
+        Default Button
+      </Button>
+      <Button type="primary" loading>
+        Primary Button
+      </Button>
+      <Button type="lead" loading>
+        Lead Button
+      </Button>
+      <Button type="gray" loading>
+        Gray Button
+      </Button>
+      <Button type="link" href="https://google.com" loading>
+        Link Button
+      </Button>
+      <br />
+      <Button type="default" work loading>
+        Default Button
+      </Button>
+      <Button type="primary" work loading>
+        Primary Button
+      </Button>
+      <Button type="primary" work workType="high-light" loading>
+        Primary Button
+      </Button>
+      <Button type="lead" work loading>
+        Lead Button
+      </Button>
+      <Button type="gray" work loading>
+        Gray Button
+      </Button>
+      <Button type="link" href="https://google.com" work loading>
+        Link Button
+      </Button>
+      <Button type="light" work loading>
+        Light Button
+      </Button>
+      <br />
+      <Button type="default" work disabled loading>
+        Default Button
+      </Button>
+      <Button type="primary" work disabled loading>
+        Primary Button
+      </Button>
+      <Button type="primary" work disabled workType="high-light" loading>
+        Primary Button
+      </Button>
+      <Button type="lead" work disabled loading>
+        Lead Button
+      </Button>
+      <Button type="gray" work disabled loading>
+        Gray Button
+      </Button>
+      <Button type="link" href="https://google.com" work disabled loading>
+        Link Button
+      </Button>
+      <Button type="light" work disabled loading>
+        Light Button
+      </Button>
+    </>
+  );
 };
 
-_LoadingComponent.storyName = '加载中 button';
+_LoadingComponent.storyName = "加载中 button";
 _LoadingComponent.parameters = {
-    controls: { hideNoControlsWarning: true },
+  controls: { hideNoControlsWarning: true },
 };
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,38 +1,46 @@
-import React, { FC } from 'react'
-import classNames from 'classnames'
-import {Button as AntdButton,  ButtonProps } from 'antd'
+import React, { FC } from "react";
+import classNames from "classnames";
+import { Button as AntdButton, ButtonProps } from "antd";
 
-import { CaretRightOutlined } from '@ant-design/icons'
+import { CaretRightOutlined } from "@ant-design/icons";
 
-export type ButtonType = 'default' | 'primary' | 'link' | 'lead' | 'gray' | 'text'
-export type ButtonWorkType = 'default' | 'high-light'
+export type ButtonType =
+  | "default"
+  | "primary"
+  | "link"
+  | "lead"
+  | "gray"
+  | "text"
+  | "light";
+export type ButtonWorkType = "default" | "high-light";
 
 interface BaseButtonProps {
   /** 设置按钮类型 */
-  type?: ButtonType
+  type?: ButtonType;
   /** 将按钮宽度调整为其父宽度的选项 */
-  block?: boolean
+  block?: boolean;
   /** 按钮失效状态 */
-  disabled?: boolean
+  disabled?: boolean;
   /** 设置 button 原生的 type 值 */
-  htmlType?: string
+  htmlType?: string;
   /** 设置按钮的图标组件 */
-  icon?: React.ReactNode
+  icon?: React.ReactNode;
   /** 设置按钮的加载状态 */
-  loading?: boolean
+  loading?: boolean;
   /** 点击跳转的地址，指定此属性 button 的行为和 a 链接一致 */
-  href?: string
+  href?: string;
   /** 相当于 a 链接的 target 属性，href 存在时生效 */
-  target?: string
+  target?: string;
   /** 设置激活状态 */
-  work?: boolean
+  work?: boolean;
   /** 设置激活状态类型，work 存在时生效（目前仅 type = default 有两种激活状态） */
-  workType?: ButtonWorkType
+  workType?: ButtonWorkType;
   /** 点击按钮时的回调 */
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export type FRCButtonProps = BaseButtonProps & Omit<ButtonProps, 'type' | 'danger'>
+export type FRCButtonProps = BaseButtonProps &
+  Omit<ButtonProps, "type" | "danger">;
 
 export const Button: FC<FRCButtonProps> = (props) => {
   const {
@@ -45,45 +53,45 @@ export const Button: FC<FRCButtonProps> = (props) => {
     workType,
     loading,
     ...restProps
-  } = props
+  } = props;
   // btn, btn-lg, btn-primary
-  const classes = classNames('frc-btn', className, {
+  const classes = classNames("frc-btn", className, {
     [`frc-btn-${type}`]: type,
     [`frc-btn-${size}`]: size,
     [`frc-btn-work-${workType}`]: work,
     [`frc-btn-loading`]: loading,
     [`frc-btn-icon-only`]: !children,
-  })
+  });
 
   const options = {
     className: classes,
     loading,
     href,
     ...restProps,
-  }
+  };
 
   // main
   return (
     <AntdButton {...options}>
       <span className="lead-child-box">
         {children}
-        {type === 'lead' && children && (
+        {type === "lead" && children && (
           <CaretRightOutlined style={{ marginLeft: 8, fontSize: 10 }} />
         )}
       </span>
     </AntdButton>
-  )
-}
+  );
+};
 
 // normal
 Button.defaultProps = {
-  type: 'default',
+  type: "default",
   block: false,
   disabled: false,
   loading: false,
   work: false,
-  workType: 'default',
-  htmlType: 'button'
-}
+  workType: "default",
+  htmlType: "button",
+};
 
-export default Button
+export default Button;
