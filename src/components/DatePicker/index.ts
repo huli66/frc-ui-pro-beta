6c8b65d90e70f1ee559dbcbb290456a73e042a63
hui.chen <hui.chen@sumscope.com>
2023-06-19 14:02:56 +0800

    feat: iteration

--- src/components/DatePicker/index.ts
+++ src/components/DatePicker/index.ts
@@ -1,13 +1,15 @@
 import { ForwardRefExoticComponent, RefAttributes } from 'react'
 import FRCDatePicker, { FRCDatePickerProps } from './datePicker'
-import FRCRangePicker from './rangePicker'
+import FRCRangePicker, { FRCRangePickerProps } from './rangePicker'
 import { PickerRef } from './interface'
 
 export type { FRCDatePickerProps, PickerRef }
-export type { FRCRangePickerProps } from './rangePicker'
+export type { FRCRangePickerProps }
 
-export type FRCDatePickerComponent = ForwardRefExoticComponent<FRCDatePickerProps & RefAttributes<PickerRef>>& {
-    RangePicker: typeof FRCRangePicker
+export type FRCDatePickerComponent = ForwardRefExoticComponent<FRCDatePickerProps & RefAttributes<PickerRef>> & {
+    RangePicker: ForwardRefExoticComponent<
+        FRCRangePickerProps & RefAttributes<PickerRef>
+    >;
 }
 
 const TransDatePicker = FRCDatePicker as FRCDatePickerComponent
