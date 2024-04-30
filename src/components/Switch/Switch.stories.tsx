8f3309686e4bb8b34fe9c3d31df703957f9a45fb
yazhong.pan <yazhong.pan@sumscope.com>
2024-04-30 11:02:05 +0800

    feat: add param extraText & extraTextPlacement for switch

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -1,189 +1,201 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
+import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
 
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta } from "@storybook/react";
 
 import {
-    Title,
-    Description,
-    ArgsTable,
-    Stories,
-    Heading,
-    Subheading
-} from '@storybook/addon-docs';
+  Title,
+  Description,
+  ArgsTable,
+  Stories,
+  Heading,
+  Subheading,
+} from "@storybook/addon-docs";
 
-import Switch, {FRCSwitchProps} from './index';
+import Switch, { FRCSwitchProps } from "./index";
 
 // ----------------------------------------------------------------
 
 // 引用示例代码
 const ImportComponent = () => {
-    const markdown = `
+  const markdown = `
 ~~~js
 import { Switch } from 'frc-ui-pro';
 
 // 按需加载 icon
 import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
 ~~~
-`
+`;
 
-    return <>
-        <ReactMarkdown children={markdown} components={{
-            code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || '')
-                return !inline && match ? (
-                    <SyntaxHighlighter
-                        children={String(children).replace(/\n$/, '')}
-                        style={tomorrow}
-                        language={match[1]}
-                        PreTag="div"
-                        {...props}
-                    />
-                ) : (
-                    <code className={className} {...props}>
-                        {children}
-                    </code>
-                )
-            }
-        }} /></>
-}
+  return (
+    <>
+      <ReactMarkdown
+        children={markdown}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={tomorrow}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </>
+  );
+};
 
 // ----------------------------------------------------------------
 
 export default {
-    title: '数据录入/Switch 开关',
-    component: Switch,
-    parameters: {
-        docs: {
-            // docs 页面 => 总体布局
-            page: () => (
-                <>
-                    <Title />
-                    <Description>开关选择器。</Description>
-                    <ImportComponent />
-                    <Stories title="组件总览" includePrimary={true} />
+  title: "数据录入/Switch 开关",
+  component: Switch,
+  parameters: {
+    docs: {
+      // docs 页面 => 总体布局
+      page: () => (
+        <>
+          <Title />
+          <Description>开关选择器。</Description>
+          <ImportComponent />
+          <Stories title="组件总览" includePrimary={true} />
 
-                    <Heading>API</Heading>
-                    <Subheading>属性</Subheading>
+          <Heading>API</Heading>
+          <Subheading>属性</Subheading>
 
-                    <Subheading>Radio/Radio.Button</Subheading>
-                    <ArgsTable of={Switch}/>
-                </>
-            ),
-        },
+          <Subheading>Radio/Radio.Button</Subheading>
+          <ArgsTable of={Switch} />
+        </>
+      ),
     },
-    // 细分属性 - 分类（用于canvas 页查阅）
+  },
+  // 细分属性 - 分类（用于canvas 页查阅）
 } as ComponentMeta<typeof Switch>;
 
 // ----------------------------------------------------------------
 
 export const Default = (args: FRCSwitchProps) => <Switch {...args} />;
 
-Default.storyName = '默认 switch';
+Default.storyName = "默认 switch";
 
 // ----------------------------------------------------------------
 
 export const _BaseComponent = () => {
-    return <>
-        <Switch />
-        <Switch checked />
-        <Switch loading />
-        <Switch loading checked />
-        <Switch disabled />
-        <Switch disabled checked />
+  return (
+    <>
+      <Switch />
+      <Switch checked />
+      <Switch loading />
+      <Switch loading checked />
+      <Switch disabled />
+      <Switch disabled checked />
     </>
-}
+  );
+};
 
-_BaseComponent.storyName = '基本使用 switch';
+_BaseComponent.storyName = "基本使用 switch";
 _BaseComponent.parameters = {
-    controls: { hideNoControlsWarning: true },
+  controls: { hideNoControlsWarning: true },
 };
 
 // ----------------------------------------------------------------
 
 export const _ChildComponent = () => {
-    return <>
-        <Switch
-            checkedChildren="开启"
-            unCheckedChildren="关闭关闭"
-        />
+  return (
+    <>
+      <Switch checkedChildren="开启" unCheckedChildren="关闭关闭" />
 
-        <Switch
-            checkedChildren="开启开启"
-            unCheckedChildren="关闭"
-            defaultChecked
-        />
+      <Switch
+        checkedChildren="开启开启"
+        unCheckedChildren="关闭"
+        defaultChecked
+      />
 
-        <Switch
-            checkedChildren="开启"
-            unCheckedChildren="关闭"
-            loading
-        />
+      <Switch checkedChildren="开启" unCheckedChildren="关闭" loading />
 
-        <Switch
-            checkedChildren="开启"
-            unCheckedChildren="关闭"
-            loading
-            checked
-        />
+      <Switch checkedChildren="开启" unCheckedChildren="关闭" loading checked />
 
-        <Switch
-            checkedChildren="开启"
-            unCheckedChildren="关闭"
-            disabled
-        />
+      <Switch checkedChildren="开启" unCheckedChildren="关闭" disabled />
 
-        <Switch
-            checkedChildren="开启"
-            unCheckedChildren="关闭"
-            disabled
-            checked
-        />
-        <br />
-        <Switch
-            checkedChildren={<CheckOutlined />}
-            unCheckedChildren={<CloseOutlined />}
-        />
+      <Switch
+        checkedChildren="开启"
+        unCheckedChildren="关闭"
+        disabled
+        checked
+      />
+      <br />
+      <Switch
+        checkedChildren={<CheckOutlined />}
+        unCheckedChildren={<CloseOutlined />}
+      />
 
-        <Switch
-            checkedChildren={<CheckOutlined />}
-            unCheckedChildren={<CloseOutlined />}
-            defaultChecked
-        />
+      <Switch
+        checkedChildren={<CheckOutlined />}
+        unCheckedChildren={<CloseOutlined />}
+        defaultChecked
+      />
 
-        <Switch
-            checkedChildren={<CheckOutlined />}
-            unCheckedChildren={<CloseOutlined />}
-            loading
-        />
+      <Switch
+        checkedChildren={<CheckOutlined />}
+        unCheckedChildren={<CloseOutlined />}
+        loading
+      />
 
-        <Switch
-            checkedChildren={<CheckOutlined />}
-            unCheckedChildren={<CloseOutlined />}
-            loading
-            checked
-        />
+      <Switch
+        checkedChildren={<CheckOutlined />}
+        unCheckedChildren={<CloseOutlined />}
+        loading
+        checked
+      />
 
-        <Switch
-            checkedChildren={<CheckOutlined />}
-            unCheckedChildren={<CloseOutlined />}
-            disabled
-        />
+      <Switch
+        checkedChildren={<CheckOutlined />}
+        unCheckedChildren={<CloseOutlined />}
+        disabled
+      />
 
-        <Switch
-            checkedChildren={<CheckOutlined />}
-            unCheckedChildren={<CloseOutlined />}
-            disabled
-            checked
-        />
+      <Switch
+        checkedChildren={<CheckOutlined />}
+        unCheckedChildren={<CloseOutlined />}
+        disabled
+        checked
+      />
     </>
-}
-
-_ChildComponent.storyName = '文字和图标';
-_ChildComponent.parameters = {
-    controls: { hideNoControlsWarning: true },
+  );
 };
+
+_ChildComponent.storyName = "文字和图标";
+_ChildComponent.parameters = {
+  controls: { hideNoControlsWarning: true },
+};
+
+// ----------------------------------------------------------------
+
+export const _ExtraTextComponent = () => {
+  return (
+    <>
+      <Switch extraText="额外信息" />
+      <Switch extraText="额外信息" extraTextPlacement="left" />
+    </>
+  );
+};
+
+_ExtraTextComponent.storyName = "switch 属性 exrtaText";
+_ExtraTextComponent.parameters = {
+  controls: { hideNoControlsWarning: true },
+};
+
+// ----------------------------------------------------------------
--- src/components/Switch/switch-core.tsx
+++ src/components/Switch/switch-core.tsx
@@ -1,46 +1,63 @@
 // 文件名为 switch，提示报错，故改为 switch-core
-import React, { FC } from 'react'
-import classNames from 'classnames'
-import { Switch as AntdSwitch, SwitchProps } from 'antd'
+import React, { FC } from "react";
+import classNames from "classnames";
+import { Switch as AntdSwitch, SwitchProps } from "antd";
 
 interface BaseSwitch {
   /** 组件自动获取焦点 */
-  autoFocus?: boolean
+  autoFocus?: boolean;
   /** 指定当前是否选中 */
-  checked?: boolean
+  checked?: boolean;
   /** 选中时的内容 */
-  checkedChildren?: React.ReactNode
+  checkedChildren?: React.ReactNode;
   /** Switch 器类名 */
-  className?: string
+  className?: string;
   /** 初始是否选中 */
-  defaultChecked?: boolean
+  defaultChecked?: boolean;
   /** 是否禁用 */
-  disabled?: boolean
+  disabled?: boolean;
   /** 加载中的开关 */
-  loading?: boolean
+  loading?: boolean;
   /** 非选中时的内容 */
-  unCheckedChildren?: React.ReactNode
+  unCheckedChildren?: React.ReactNode;
   /** 变化时回调函数 */
-  onChange?: (checked: boolean, event: Event) => void
+  onChange?: (checked: boolean, event: Event) => void;
   /** 点击时回调函数 */
-  onClick?: (checked: boolean, event: Event) => void
+  onClick?: (checked: boolean, event: Event) => void;
+  /** extraText 额外文字提示 */
+  extraText?: string;
+  /** extraTextPlacement 额外文字提示位置 默认right */
+  extraTextPlacement?: "left" | "right";
 }
 
-export type FRCSwitchProps = BaseSwitch & SwitchProps
+export type FRCSwitchProps = BaseSwitch & SwitchProps;
 
 export const Switch: FC<FRCSwitchProps> = (props) => {
-  const { className, ...restProps } = props
+  const { className, extraText, extraTextPlacement, ...restProps } = props;
 
-  const classes = classNames('frc-switch', className, {})
+  const classes = classNames("frc-switch", className, {});
 
   const options = {
     className: classes,
     ...restProps,
+  };
+  if (extraText) {
+    const placement = extraTextPlacement || "right";
+    return (
+      <div
+        className={classNames("frc-switch-with-extra-text", {
+          "flex-reverse": placement === "left",
+        })}
+      >
+        <AntdSwitch {...options} />
+        <span className="frc-switch-extra-text">{extraText}</span>
+      </div>
+    );
   }
 
   // main
-  return <AntdSwitch {...options} />
-}
+  return <AntdSwitch {...options} />;
+};
 
 // normal
 Switch.defaultProps = {
@@ -48,6 +65,6 @@ Switch.defaultProps = {
   defaultChecked: false,
   disabled: false,
   loading: false,
-}
+};
 
-export default Switch
+export default Switch;
